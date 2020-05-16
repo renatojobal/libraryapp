b0d1c1affff3dd7ddcf94798abb1bd6ce7ee87d5
renatojobal <renatojobal@gmail.com>
2020-05-16 15:50:33 -0500

    Binding the search view query to a method

--- app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/SearchBookViewModel.java
+++ app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/SearchBookViewModel.java
@@ -1,6 +1,8 @@
 package com.renatojobal.libraryutpl.mainactivity.fsearchbook;
 
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.paging.DataSource;
@@ -10,6 +12,7 @@ import androidx.paging.PagedList;
 import com.renatojobal.libraryutpl.repository.localdatabase.RoomHelper;
 import com.renatojobal.libraryutpl.repository.model.SampleBookModel;
 
+import java.util.List;
 
 
 public class SearchBookViewModel extends ViewModel {
@@ -18,23 +21,35 @@ public class SearchBookViewModel extends ViewModel {
      *
      * @param sampleBookList list of books found
      */
+    private static final String TAG = "SearchBookViewModel";
 
-    private LiveData<PagedList<SampleBookModel>> sampleBookResultList;
+    private LiveData<List<SampleBookModel>> sampleBookResultList;
 
-    // Data source
-    DataSource.Factory<Integer, SampleBookModel> myResultDataSource;
+    public String targetBook;
 
     public SearchBookViewModel() {
 
-        myResultDataSource =
-                RoomHelper.getAppDatabaseInstance().sampleBookDao().getLiveDataPagedList();
+        sampleBookResultList = RoomHelper.getAppDatabaseInstance().sampleBookDao().getLiveDataList();
 
-        sampleBookResultList =
-                new LivePagedListBuilder<Integer, SampleBookModel>(
-                RoomHelper.getAppDatabaseInstance().sampleBookDao().getLiveDataPagedList(),
-                20)
-                .build();
+
     }
+
+    public LiveData<List<SampleBookModel>> getSampleBookResultList() {
+        /** Expose the result list*/
+        return sampleBookResultList;
+    }
+
+
+    // UI interaction
+    public void setTargetBook(String query){
+        /**
+         * Notify the fragment
+         */
+        // TODO: Do the request to the api and then storage it into the database
+
+    }
+
+
 
 
 
