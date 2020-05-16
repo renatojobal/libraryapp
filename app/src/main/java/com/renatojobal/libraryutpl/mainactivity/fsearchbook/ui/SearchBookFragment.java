15e61ad16664207c973d84ca79e034ff12a1dd35
renatojobal <renatojobal@gmail.com>
2020-05-16 14:38:15 -0500

    Configurated the recycler view

--- app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/ui/SearchBookFragment.java
+++ app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/ui/SearchBookFragment.java
@@ -1,6 +1,7 @@
 package com.renatojobal.libraryutpl.mainactivity.fsearchbook.ui;
 
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,9 @@ import com.renatojobal.libraryutpl.R;
 
 import com.renatojobal.libraryutpl.databinding.FragmentSearchBookBinding;
 import com.renatojobal.libraryutpl.mainactivity.fsearchbook.SearchBookViewModel;
+import com.renatojobal.libraryutpl.repository.model.SampleBookModel;
+
+import java.util.List;
 
 public class SearchBookFragment extends Fragment {
     /**
@@ -27,21 +32,52 @@ public class SearchBookFragment extends Fragment {
 
     FragmentSearchBookBinding binding;
 
+    // UI elements (Could use dagger for injection in hte future)
+    private RecyclerView resultRecyclerView;
+    private SamplesBookLiveDataListAdapter samplesBookLiveDataListAdapter;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        /**
+         * Link the view model
+         */
+        super.onCreate(savedInstanceState);
+        searchBookViewModel = new ViewModelProvider(this).get(SearchBookViewModel.class);
+
+
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_book, container, false);
+
+        samplesBookLiveDataListAdapter = new SamplesBookLiveDataListAdapter(getContext(), searchBookViewModel.getSampleBookResultList());
+
+        searchBookViewModel.getSampleBookResultList().observe(getViewLifecycleOwner(), new Observer<List<SampleBookModel>>() {
+            @Override
+            public void onChanged(List<SampleBookModel> sampleBookModelList) {
+                binding.recyclerViewResultList.setAdapter(samplesBookLiveDataListAdapter);
+            }
+        });
+
+
+
         return binding.getRoot();
+
+
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        searchBookViewModel = new ViewModelProvider(this).get(SearchBookViewModel.class);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        /**
+         * Change Live Data into the observer.
+         */
+        super.onViewCreated(view, savedInstanceState);
 
-
+        // Linking UI elements
 
 
     }
-
 }
