6dbcfe4a94765f32755e117f0fbafd36b8cbcd9a
renatojobal <renatojobal@gmail.com>
2020-05-17 10:43:03 -0500

    Fixed not init thread

--- app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/SearchBookPresenter.java
+++ app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fsearchbook/SearchBookPresenter.java
@@ -129,6 +129,6 @@ public class SearchBookPresenter {
                 RoomHelper.getAppDatabaseInstance().shelfDao().deleteAll();
                 RoomHelper.getAppDatabaseInstance().sampleBookDao().deleteAll();
             }
-        });
+        }).start();
     }
 }
