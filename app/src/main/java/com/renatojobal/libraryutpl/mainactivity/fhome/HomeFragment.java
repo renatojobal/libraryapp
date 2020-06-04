e053dd9d00a8b3a5ed79f844ee170dadcc4e1caf
renatojobal <renatojobal@gmail.com>
2020-06-04 10:50:03 -0500

    Setting up the data binding

--- app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fhome/HomeFragment.java
+++ app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fhome/HomeFragment.java
@@ -9,10 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.ActionOnlyNavDirections;
 
 import com.renatojobal.libraryutpl.R;
 import com.renatojobal.libraryutpl.databinding.FragmentHomeBinding;
+import com.renatojobal.libraryutpl.mainactivity.MainViewModel;
+import com.renatojobal.libraryutpl.mainactivity.fhome.ui.RootRecyclerViewAdapter;
 
 
 public class HomeFragment extends Fragment {
@@ -23,8 +27,14 @@ public class HomeFragment extends Fragment {
     // Fragment view-model
     HomeViewModel homeViewModel;
 
+    // Main vie-model
+    MainViewModel mainViewModel;
+
     // Using data binding
-    FragmentHomeBinding homeBinding;
+    FragmentHomeBinding binding;
+
+    // Adapters
+    RootRecyclerViewAdapter rootRecyclerViewAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +44,7 @@ public class HomeFragment extends Fragment {
          */
         super.onCreate(savedInstanceState);
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
     }
 
 
@@ -43,11 +54,33 @@ public class HomeFragment extends Fragment {
         /**
          * onCreateView
          */
-        homeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        setUpBindingData();
+
+        // Create the root adapter
+        // TODO: Set up the adapters
+        rootRecyclerViewAdapter = new RootRecyclerViewAdapter();
+
 
 
 
         // Return the binding root
-        return homeBinding.getRoot();
+        return binding.getRoot();
     }
+
+    private void setUpBindingData(){
+        binding.setMainViewModel(mainViewModel);
+        binding.setHomeViewModel(homeViewModel);
+        setUpNavigation();
+    }
+
+
+    private void setUpNavigation(){
+        binding.setNavDirection(HomeFragmentDirections.actionHomeFragmentToSearchBookFragment());
+
+    }
+
+
+
+
 }
