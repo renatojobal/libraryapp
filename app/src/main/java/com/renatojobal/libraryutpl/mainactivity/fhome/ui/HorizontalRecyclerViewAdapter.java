48e070c9b4f519e2200396b2f54c69b7e5f5b919
renatojobal <renatojobal@gmail.com>
2020-06-22 08:44:44 -0500

    Showing books in the home fragment

--- app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fhome/ui/HorizontalRecyclerViewAdapter.java
+++ app/src/main/java/com/renatojobal/libraryutpl/mainactivity/fhome/ui/HorizontalRecyclerViewAdapter.java
@@ -1,12 +1,19 @@
 package com.renatojobal.libraryutpl.mainactivity.fhome.ui;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.renatojobal.libraryutpl.R;
+import com.renatojobal.libraryutpl.databinding.ItemHomeBookBinding;
+import com.renatojobal.libraryutpl.databinding.ItemHorizontalHomeListBinding;
 import com.renatojobal.libraryutpl.repository.model.BookInfoModel;
 
 import java.util.List;
@@ -19,33 +26,102 @@ public class HorizontalRecyclerViewAdapter extends RecyclerView.Adapter<Horizont
      * @return
      */
 
+    // Context
+    Context context;
+
     // Data source
-    LiveData<List<BookInfoModel>> list;
+    List<BookInfoModel> list;
+
+    /**
+     * Constructor
+     * @param context
+     * @param horizontalList
+     */
+    public HorizontalRecyclerViewAdapter(Context context, List<BookInfoModel> horizontalList) {
+        this.context = context;
+        this.list = horizontalList;
+    }
 
 
+
+    /**
+     * Create new views (invoked by the layout manager)
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public HorizontalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+        ItemHomeBookBinding binding = DataBindingUtil.inflate(layoutInflater,
+                R.layout.item_home_book, parent, false);
+
+
+        // Return the view holder with the elements attached
+       HorizontalRecyclerViewAdapter.HorizontalViewHolder viewHolder = new HorizontalRecyclerViewAdapter.HorizontalViewHolder(binding);
+
+        return viewHolder;
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull HorizontalViewHolder holder, int position) {
+        // - get element from your data set at this position
+        BookInfoModel bookInfoModel = getItem(position);
 
+        // - replace the contents of the view with that element
+        holder.bind(bookInfoModel);
+    }
+
+    /**
+     * Return the item from the data source
+     * @param position
+     * @return
+     */
+    private BookInfoModel getItem(int position) {
+        return this.list.get(position);
     }
 
     @Override
     public int getItemCount() {
-        return list.getValue().size();
+        return list.size();
     }
+
 
 
     /**
      * View holder for each book
      */
     public class HorizontalViewHolder extends RecyclerView.ViewHolder {
-        public HorizontalViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        // Using with data binding
+        ItemHomeBookBinding itemHomeBookBinding;
+
+        /**
+         * Constructor with bind data
+         * @param binding
+         */
+        public HorizontalViewHolder(ItemHomeBookBinding binding) {
+            super(binding.getRoot());
+            this.itemHomeBookBinding = binding;
+
         }
+
+        /**
+         * Bind the list with each item
+         * @param bookInfoModel
+         */
+        public void bind(BookInfoModel bookInfoModel) {
+            itemHomeBookBinding.setBookInfoModel(bookInfoModel);
+
+        }
+
+
     }
 }
