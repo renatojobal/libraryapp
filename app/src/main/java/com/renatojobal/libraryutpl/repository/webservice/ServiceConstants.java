4fe254d04a533bdec7732ca33faf36b0e1c7bd15
Renato <renatojobal@gmail.com>
2020-09-03 10:46:41 -0500

    Fixing styles

--- app/src/main/java/com/renatojobal/libraryutpl/repository/webservice/ServiceConstants.java
+++ app/src/main/java/com/renatojobal/libraryutpl/repository/webservice/ServiceConstants.java
@@ -5,8 +5,8 @@ package com.renatojobal.libraryutpl.repository.webservice;
  */
 public class ServiceConstants {
 
-    //public static final String BASE_URL = "http://18.231.100.232:8000";
-    public static final String BASE_URL = "http://10.0.2.2:8000";
+    public static final String BASE_URL = "http://15.228.58.193:8000";
+    // public static final String BASE_URL = "http://10.0.2.2:8000";
     public static final String SEARCH_BOOK_BY_TITLE_URL = "/api/movilapp/search_book_by_title/";
     public static final String PULL_RECOMMENDED_BOOKS = "/api/movilapp/get_recommended_lists/";
 }
