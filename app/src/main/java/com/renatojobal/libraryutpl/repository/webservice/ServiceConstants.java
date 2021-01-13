3e0aaca1b539f7468d17942dba909ab7bfd9b7da
renatojobal <renatojobal@gmail.com>
2021-01-13 12:46:00 -0500

    fix: update url

--- app/src/main/java/com/renatojobal/libraryutpl/repository/webservice/ServiceConstants.java
+++ app/src/main/java/com/renatojobal/libraryutpl/repository/webservice/ServiceConstants.java
@@ -5,7 +5,7 @@ package com.renatojobal.libraryutpl.repository.webservice;
  */
 public class ServiceConstants {
 
-    public static final String BASE_URL = "http://15.228.58.193:8000";
+    public static final String BASE_URL = "http://15.228.74.142:8000/";
     // public static final String BASE_URL = "http://10.0.2.2:8000"; // For development
     public static final String SEARCH_BOOK_BY_TITLE_URL = "/api/movilapp/search_book_by_title/";
     public static final String PULL_RECOMMENDED_BOOKS = "/api/movilapp/get_recommended_lists/";
